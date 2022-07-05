6d2897b78304d72e491b58da55c24da3c44b8b09
iliyanCoding <iliyan.programirane@gmail.com>
2022-07-05 15:33:37 +0300

    Create RandomList.cs

---
+++ Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    internal class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            string rndStr = string.Empty;
+            Random rnd = new Random();
+            int index = rnd.Next(0, this.Count);
+            rndStr = this[index];
+            this.RemoveAt(index);
+            return rndStr;
+        }
+    }
+}
