fd4120d74304abf4d61f0dafa81f7af8dba7b7a3
iliyanCoding <iliyan.programirane@gmail.com>
2022-07-07 10:36:12 +0300

    Create StackOfStrings.cs

---
+++ Inheritance/CustomStack/StackOfStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count > 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void AddRange(IEnumerable<string> el)
+        {
+            foreach (var str in el)
+            {
+                this.Push(str);
+            }
+        }
+    }
+}
