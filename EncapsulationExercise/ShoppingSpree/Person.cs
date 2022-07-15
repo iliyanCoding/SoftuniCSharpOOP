f82fc9dfe1e2668fe52d3c731dbd67fed3b80b6c
iliyanCoding <iliyan.programirane@gmail.com>
2022-07-15 10:11:25 +0300

    Create Person.cs

---
+++ EncapsulationExercise/ShoppingSpree/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        private List<Product> bagOfProducts;
+
+        public Person(string name, int age, List<Person> bagOfProducts)
+        {
+            this.name = name;
+            this.age = age;
+            this.bagOfProducts = new List<Product>();
+        }
+
+        public string Name
+        {
+            get 
+            {
+                return this.name; 
+            }
+            private set 
+            {
+                if (value == string.Empty)
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value; 
+            }
+        }
+
+        public int Age
+        {
+            get 
+            {
+                return this.age; 
+            }
+            private set
+            {
+                this.age = value;
+            }
+        }
+
+    }
+}
