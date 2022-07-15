19d2102acd76d524bc19fbb01de23b990f1d75d2
iliyanCoding <iliyan.programirane@gmail.com>
2022-07-16 00:14:17 +0300

    Create Program.cs

---
+++ EncapsulationExercise/ShoppingSpree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] people = Console.ReadLine().Split(';');
+            string[] products = Console.ReadLine().Split(';');
+            for (int i = 0; i < people.Length; i++)
+            {
+                
+            }
+        }
+    }
+}
