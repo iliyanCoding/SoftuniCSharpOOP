a1c1115782abb4f66940673ce2c5b51bfefdd0a4
iliyanCoding <iliyan.programirane@gmail.com>
2022-07-15 10:11:28 +0300

    Create Product.cs

---
+++ EncapsulationExercise/ShoppingSpree/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Product
+    {
+        private string name;
+        private int cost; 
+
+        public string Name
+        {
+            get
+            {
+                return this.name; 
+            }
+            private set 
+            {
+                if (value == string.Empty)
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value; 
+            }
+        }
+
+        public int Cost
+        {
+            get 
+            { 
+                return this.cost; 
+            }
+            private set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.cost = value; 
+            }
+        }
+
+    }
+}
