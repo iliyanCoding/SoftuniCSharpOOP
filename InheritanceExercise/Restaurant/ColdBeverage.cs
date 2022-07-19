1826017b636df367c69e0eb1db1ffbcd5d1facce
iliyanCoding <iliyan.programirane@gmail.com>
2022-07-19 11:44:02 +0300

    Create ColdBeverage.cs

---
+++ InheritanceExercise/Restaurant/ColdBeverage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    internal class ColdBeverage : Beverage
+    {
+        public ColdBeverage(string name, decimal price, double milliliters) : base(name, price, milliliters)
+        {
+
+        }
+    }
+}
