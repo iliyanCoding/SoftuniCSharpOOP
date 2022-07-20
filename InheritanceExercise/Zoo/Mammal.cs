d5ff9123e411d4531aa55b9cec9a2ca5bd3f77d1
iliyanCoding <iliyan.programirane@gmail.com>
2022-07-20 14:01:05 +0300

    Create Mammal.cs

---
+++ InheritanceExercise/Zoo/Mammal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    internal class Mammal : Animal
+    {
+        public Mammal(string name) : base(name)
+        {
+
+        }
+    }
+}
