24dc22a269456568cd9c107ca31cc2f824e6a1c1
iliyanCoding <iliyan.programirane@gmail.com>
2022-07-20 14:01:02 +0300

    Create Lizard.cs

---
+++ InheritanceExercise/Zoo/Lizard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    internal class Lizard : Reptile
+    {
+        public Lizard(string name) : base(name)
+        {
+
+        }
+    }
+}
