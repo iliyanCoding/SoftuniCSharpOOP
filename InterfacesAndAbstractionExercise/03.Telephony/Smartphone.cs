2cb158fb89f3bc3397e86ff82873ffaf98e77297
iliyanCoding <iliyan.programirane@gmail.com>
2022-07-30 14:27:39 +0300

    Create Smartphone.cs

---
+++ InterfacesAndAbstractionExercise/03.Telephony/Smartphone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Telephony
+{
+    public class Smartphone : IBrowseable, ICallable
+    {
+        public string PhoneNumber { get; set; }
+
+        public string Url { get; set; }
+
+        public Smartphone(string phoneNumber)
+        {
+            this.PhoneNumber = phoneNumber;
+        }
+
+        public void Browse(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Call(string phoneNumber)
+        {
+            return $"Calling... {phoneNumber}";
+        }
+    }
+}
