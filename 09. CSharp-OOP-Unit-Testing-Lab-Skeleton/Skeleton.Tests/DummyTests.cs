1eea4b820f499bf597f40bdb1fb263eee7e7dccb
iliyanCoding <iliyan.programirane@gmail.com>
2022-07-31 11:49:11 +0300

    Create DummyTests.cs

---
+++ 09. CSharp-OOP-Unit-Testing-Lab-Skeleton/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLosesHealthIfAttacked()
+        {
+            Dummy dummy = new Dummy(10, 10);
+            Axe axe = new Axe(10, 10);
+
+            axe.Attack(dummy);
+
+            Assert.That(dummy.Health, Is.EqualTo(0), "Dummy loses health if attacked.");
+        }
+
+        [Test]
+        public void DeadDummyThrowsExceptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(10, 10);
+            Axe axe = new Axe(10, 10);
+
+            axe.Attack(dummy);
+            axe.Attack(dummy);
+
+            Assert.That(dummy.Health)
+        }
+    }
+}
