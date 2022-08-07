61f4ebfaffd2e76f9080b88f9be52fb19305b87f
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-07 12:59:11 +0300

    Create StationaryPhone.cs

---
+++ InterfacesAndAbstractionExercise/03.Telephony/StationaryPhone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Telephony
+{
+    public class StationaryPhone : ICallable
+    {
+        public string PhoneNumber { get; set; }
+        public StationaryPhone(string phoneNumber)
+        {
+            this.PhoneNumber = phoneNumber;
+        }
+        public string Call(string phoneNumber)
+        {
+            return $"Dialing... {phoneNumber}";
+        }
+    }
+}
