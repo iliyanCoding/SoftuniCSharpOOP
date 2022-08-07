d2df2a8dbaa54293794a5053675e3e83c1520e5a
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-07 12:59:39 +0300

    Create Pet.cs

---
+++ InterfacesAndAbstractionExercise/04.BorderControl/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.BorderControl
+{
+    public class Pet : IBirthable
+    {
+        public Pet(string birthdate)
+        {
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get; private set; }
+
+        public string Birthdate { get; private set; }
+    }
+}
