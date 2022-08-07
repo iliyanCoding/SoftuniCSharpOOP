c4cac318984a1d3c24f981a40c0b6cbbc968708b
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-07 12:59:21 +0300

    Create Citizen.cs

---
+++ InterfacesAndAbstractionExercise/04.BorderControl/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.BorderControl
+{
+    public class Citizen : IBirthable
+    {
+        public Citizen(string birthdate)
+        {
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get; private set; }
+
+        public string Age { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string Birthdate { get; private set; }
+    }
+}
