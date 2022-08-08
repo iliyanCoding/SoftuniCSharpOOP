08bbd4fbf66776a6f31bca11e7a33720c53956bd
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-08 10:15:04 +0300

    Create Robot.cs

---
+++ InterfacesAndAbstractionExercise/04.BorderControl/Robot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.BorderControl
+{
+    public class Robot : IIdentifiable
+    {
+        public Robot(string id)
+        {
+            this.Id = id;
+        }
+        public string Model { get; private set; }
+        public string Id { get; private set; }
+    }
+}
