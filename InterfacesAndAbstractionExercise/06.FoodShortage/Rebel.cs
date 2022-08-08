a9db500da7d7d4d4285e610537b912dc1ceefaf2
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-08 10:15:21 +0300

    Create Rebel.cs

---
+++ InterfacesAndAbstractionExercise/06.FoodShortage/Rebel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.FoodShortage
+{
+    public class Rebel : IBuyer
+    {
+        public Rebel(string name, string age, string group)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Group = group;
+        }
+
+        public string Name { get; private set; }
+
+        public string Age { get; private set; }
+
+        public string Group { get; private set; }
+
+        public int Food { get; set; } = 0;
+
+        public void BuyFood()
+        {
+            this.Food += 5;
+        }
+    }
+}
