10a3b9c24ae35f52f19f9fa67b48910ead14df1c
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-08 10:15:18 +0300

    Create IBuyer.cs

---
+++ InterfacesAndAbstractionExercise/06.FoodShortage/IBuyer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.FoodShortage
+{
+    public interface IBuyer
+    {
+        public int Food { get; set; }
+
+        void BuyFood();
+    }
+}
