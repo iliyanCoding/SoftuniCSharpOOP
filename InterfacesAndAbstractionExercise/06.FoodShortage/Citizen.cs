fdd22b053d93f2962bb0962b2dc691da9b703b46
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-08 10:15:16 +0300

    Create Citizen.cs

---
+++ InterfacesAndAbstractionExercise/06.FoodShortage/Citizen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.FoodShortage
+{
+    public class Citizen : IBuyer
+    {
+        public Citizen(string name, string age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get; private set; }
+
+        public string Age { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string Birthdate { get; private set; }
+
+        public int Food { get; set; } = 0;
+
+        public void BuyFood()
+        {
+            this.Food += 10;
+        }
+    }
+}
