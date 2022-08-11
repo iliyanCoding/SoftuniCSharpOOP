fba198ca3683f54a75d06e714739524b32f22875
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-11 23:26:11 +0300

    Create Truck.cs

---
+++ PolimorphismExercise/01.Vehicles/Truck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Vehicles
+{
+    public class Truck : Vehicle
+    {
+        private const double TruckFuelConsumptionIncreasement = 1.6;
+        public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity)
+            : base(fuelQuantity, fuelConsumption + TruckFuelConsumptionIncreasement, tankCapacity)
+        {
+        }
+
+        public override void Refuel(double liters)
+        {
+            if (this.FuelQuantity + liters > this.TankCapacity)
+            {
+                Console.WriteLine($"Cannot fit {liters} fuel in the tank");
+            }
+            else
+            {
+                this.FuelQuantity += 0.95 * liters;
+            }
+        }
+    }
+}
