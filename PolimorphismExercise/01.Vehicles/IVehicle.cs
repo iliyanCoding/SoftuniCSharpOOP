858b84966147722afdbcc083c97a21d9170944e1
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-11 23:26:05 +0300

    Create IVehicle.cs

---
+++ PolimorphismExercise/01.Vehicles/IVehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Vehicles
+{
+    public interface IVehicle
+    {
+        public double FuelQuantity { get; }
+
+        public double FuelConsumption { get; }
+
+        public double TankCapacity { get; }
+
+        string Drive(double distance);
+
+        void Refuel(double liters);
+    }
+}
