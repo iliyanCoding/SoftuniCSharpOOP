6df020bb69b7d80d237067b36629b48076833629
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-11 23:26:16 +0300

    Create Vehicle.cs

---
+++ PolimorphismExercise/01.Vehicles/Vehicle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Vehicles
+{
+    public abstract class Vehicle : IVehicle
+    {
+        public Vehicle(double fuelQuantity, double fuelConsumption, double tankCapacity)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+            this.TankCapacity = tankCapacity;
+        }
+
+        public double FuelQuantity { get; protected set; }
+
+        public double FuelConsumption { get; protected set; }
+
+        public double TankCapacity { get; protected set; }
+
+        public string Drive(double distance)
+        {
+            if (distance * FuelConsumption > FuelQuantity)
+            {
+                return $"{this.GetType().Name} needs refueling";
+            }
+
+            this.FuelQuantity -= distance * this.FuelConsumption;
+            return $"{this.GetType().Name} travelled {distance} km";
+        }
+
+        public virtual void Refuel(double liters)
+        {
+            if (this.FuelQuantity + liters > this.TankCapacity)
+            {
+                Console.WriteLine($"Cannot fit {liters} fuel in the tank");
+            }
+            else
+            {
+                this.FuelQuantity += liters;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}: {this.FuelQuantity:f2}";
+        }
+    }
+}
