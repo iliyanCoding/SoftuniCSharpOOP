1b41db469c0343bdec13ed5abe938fbcca97c7f6
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-11 23:26:03 +0300

    Create Car.cs

---
+++ PolimorphismExercise/01.Vehicles/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Vehicles
+{
+    public class Car : Vehicle
+    {
+        private const double CarFuelConsumptionIncreasement = 0.9;
+        public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) 
+            : base(fuelQuantity, fuelConsumption + CarFuelConsumptionIncreasement, tankCapacity)
+        {
+        }
+
+        public override void Refuel(double liters)
+        {
+            this.FuelQuantity += liters;
+        }
+
+    }
+}
