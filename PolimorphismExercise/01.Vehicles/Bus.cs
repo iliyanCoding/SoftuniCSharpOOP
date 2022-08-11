c1cfad7c2fc1ada0c57c9abc94182e2dd270a45c
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-11 23:26:01 +0300

    Create Bus.cs

---
+++ PolimorphismExercise/01.Vehicles/Bus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Vehicles
+{
+    public class Bus : Vehicle
+    {
+        private const double BusFuelConsumptionIncreasment = 1.4;
+        public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) 
+            : base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+        }
+
+        //public string Drive(double distance)
+        //{
+
+        //}
+    }
+}
