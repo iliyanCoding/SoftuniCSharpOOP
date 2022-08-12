fb98dd5b3ff24a7265ecbe728a4a2b3bd73a1064
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-12 13:40:03 +0300

    Create Rogue.cs

---
+++ PolimorphismExercise/03.Raiding/Rogue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Raiding
+{
+    public class Rogue : BaseHero
+    {
+        private const int RoguePower = 80;
+
+        public Rogue(string name) : base(name)
+        {
+            this.Power = RoguePower;
+        }
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {this.Name} hit for {this.Power} damage";
+        }
+    }
+}
