b54bd0a4019941bd08f09ba9f70a694f9d8269fe
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-12 13:39:52 +0300

    Create BaseHero.cs

---
+++ PolimorphismExercise/03.Raiding/BaseHero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Raiding
+{
+    public abstract class BaseHero
+    {
+        public BaseHero(string name)
+        {
+            this.Name = name;
+            //this.Power = power;
+        }
+
+        public string Name { get; private set; }
+
+        public int Power { get; protected set; }
+
+        public virtual string CastAbility()
+        {
+            return null;
+        }
+    }
+}
