711ed14a8d33130989ceeee832b15467e7311419
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-12 13:39:56 +0300

    Create Paladin.cs

---
+++ PolimorphismExercise/03.Raiding/Paladin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Raiding
+{
+    public class Paladin : BaseHero
+    {
+        private const int PaladinPower = 100;
+        public Paladin(string name) : base(name)
+        {
+            this.Power = PaladinPower;
+        }
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {this.Name} healed for {this.Power}";
+        }
+    }
+}
