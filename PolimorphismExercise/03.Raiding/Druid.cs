fa712f62fdd5bc720dc2ba5f9f3b6d730a665146
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-12 13:39:54 +0300

    Create Druid.cs

---
+++ PolimorphismExercise/03.Raiding/Druid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Raiding
+{
+    public class Druid : BaseHero
+    {
+        private const int DruidPower = 80;
+
+        public Druid(string name) : base(name)
+        {
+            this.Power = DruidPower;
+        }
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {this.Name} healed for {this.Power}";
+        }
+    }
+}
