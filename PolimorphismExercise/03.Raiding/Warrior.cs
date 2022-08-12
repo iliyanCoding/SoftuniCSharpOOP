e05cd18840f9d5c9f682529c7ec676fe7d1fc8d1
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-12 13:40:05 +0300

    Create Warrior.cs

---
+++ PolimorphismExercise/03.Raiding/Warrior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Raiding
+{
+    public class Warrior : BaseHero
+    {
+        private const int WarriorPower = 100;
+
+        public Warrior(string name) : base(name)
+        {
+            this.Power = WarriorPower;
+        }
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {this.Name} hit for {this.Power} damage";
+        }
+    }
+}
