33b55b30892dcf324c8c5f9d39b770dbc60a4dd3
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-18 11:10:33 +0300

    Create Shape.cs

---
+++ Polymorphism/03.Shapes/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    abstract class Shape
+    {
+        public abstract double CalculatePerimeter()
+        {
+
+        }
+    }
+}
