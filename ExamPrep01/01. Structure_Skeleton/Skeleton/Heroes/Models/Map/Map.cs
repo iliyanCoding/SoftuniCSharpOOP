decdb7afb5b66853dc75ca597e13f3d1d4b53fa5
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-21 20:34:55 +0300

    Create Map.cs

---
+++ ExamPrep01/01. Structure_Skeleton/Skeleton/Heroes/Models/Map/Map.cs
@@ -0,0 +1,15 @@
+﻿using Heroes.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Map
+{
+    public class Map : IMap
+    {
+        public string Fight(ICollection<IHero> players)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
