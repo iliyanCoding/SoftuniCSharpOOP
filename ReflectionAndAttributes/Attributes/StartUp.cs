55a209dfee367b500e73ff4fe6173abefac140bf
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-22 07:48:29 +0300

    Create StartUp.cs

---
+++ ReflectionAndAttributes/Attributes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Attributes
+{
+    [Author("Victor")]
+    public class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
