d8a3afc665f36878070c1d4826d267ba8fb80293
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-23 21:53:16 +0300

    Create Claymore.cs

---
+++ ExamPrep01/01. Structure_Skeleton/Skeleton/Heroes/Models/Weapons/Claymore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    public class Claymore : Weapon
+    {
+        public Claymore(string name, int durability) : base(name, durability)
+        {
+
+        }
+
+        public override int DoDamage()
+        {
+            this.Durability--;
+            if (this.Durability == 0)
+            {
+                return 0;
+            }
+
+            return 25;
+        }
+    }
+}
