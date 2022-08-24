d3b3f3cb3a9d47c2dc4f3492d534bec1bcb73250
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-24 19:16:49 +0300

    Create Ornament.cs

---
+++ ExamPrep02/AquaShop/Models/Decorations/Ornament.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Decorations
+{
+    public class Ornament : Decoration
+    {
+        private const int OrnamentComfort = 1;
+        private const decimal OrnamentPrice = 5m;
+        public Ornament() 
+            : base(OrnamentComfort, OrnamentPrice)
+        {
+
+        }
+
+    }
+}
