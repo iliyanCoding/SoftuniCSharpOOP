c0ef42cf77f6cadb1ab938f152c405a4958c7e6f
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-24 19:16:31 +0300

    Create DeclarationRepository.cs

---
+++ ExamPrep02/AquaShop/Repositories/DeclarationRepository.cs
@@ -0,0 +1,26 @@
+﻿using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Repositories
+{
+    public class DeclarationRepository : IRepository<IDecoration>
+    {
+        private readonly ICollection<IDecoration> models;
+
+        public DeclarationRepository()
+        {
+            models = new HashSet<IDecoration>();
+        }
+        public IReadOnlyCollection<IDecoration> Models => (IReadOnlyCollection<IDecoration>) this.models;
+
+        public void Add(IDecoration model) => this.models.Add(model);
+
+        public IDecoration FindByType(string type) => this.models.FirstOrDefault(x => x.GetType().Name == type)
+
+        public bool Remove(IDecoration model) => this.models.Remove(model);
+    }
+}
