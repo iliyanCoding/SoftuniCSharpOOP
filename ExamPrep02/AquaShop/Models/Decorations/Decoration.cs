5692efab0f613698ee1f11ee5bae6f0d90b46cff
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-30 17:09:31 +0300

    Create Decoration.cs

---
+++ ExamPrep02/AquaShop/Models/Decorations/Decoration.cs
@@ -0,0 +1,44 @@
+﻿using AquaShop.Models.Decorations.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Decorations
+{
+    public abstract class Decoration : IDecoration
+    {
+        public Decoration(int comfort, decimal price)
+        {
+            this.Comfort = comfort;
+            this.Price = price;
+        }
+
+        private int comfort;
+        private decimal price;
+
+        public int Comfort
+        {
+            get 
+            {
+                return comfort; 
+            }
+            private set 
+            { 
+                comfort = value; 
+            }
+        }
+
+        public decimal Price 
+        { 
+            get 
+            { 
+                return price; 
+            }
+            set
+            {
+                price = value;
+            } 
+        }
+
+    }
+}
