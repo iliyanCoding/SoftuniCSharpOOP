47426f39a9228e58358b5a9cd6edd03800936b92
iliyanCoding <iliyan.programirane@gmail.com>
2022-08-30 17:09:22 +0300

    Create FreshwaterAquarium.cs

---
+++ ExamPrep02/AquaShop/Models/Aquariums/FreshwaterAquarium.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Aquariums
+{
+    public class FreshwaterAquarium : Aquarium
+    {
+        private const int FreshWaterAquariumCapacity = 50;
+
+        public FreshwaterAquarium(string name) : base(name, FreshWaterAquariumCapacity)
+        {
+
+        }
+    }
+}
